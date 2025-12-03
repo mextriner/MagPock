2a916eb3d973cb836ca0b64dd4c9b74912556db9
Máximo Mestriner <maximo.mestriner@alten.es>
2025-12-03 12:21:49 +0100

    feat: estrucura inicial del dash board

---
+++ src/app/dashboard/pages/control-flow/control-flow.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-control-flow',
+  imports: [],
+  templateUrl: './control-flow.html',
+  styles: ``,
+})
+export class ControlFlow { }
